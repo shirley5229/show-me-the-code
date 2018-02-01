a0e6e67ebd678dfd418350fc73d64e77d90fb160
mengxue <shirley5229@126.com>
2018-02-01 15:33:57 +0800

    更新

---
+++ 0001/0001.py
@@ -0,0 +1,16 @@
+import random, string
+
+forSelect = string.ascii_letters + string.digits
+
+def generate(count, length):
+    # count = 200
+    # length = 20
+
+    for x in range(count):
+        Re = ""
+        for y in range(length):
+            Re += random.choice(forSelect)
+        print(Re)
+
+if __name__ == "__main__":
+    generate(200, 20)
